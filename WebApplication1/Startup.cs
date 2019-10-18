479a0fb6e8fd035515f2755c3cb711799be7dfe8
chekulhan <46518731+chekulhanseim@users.noreply.github.com>
2019-10-18 11:11:30 +0200

    Mis cambios 1

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -11,6 +11,7 @@ namespace WebApplication1
 {
     public class Startup
     {
+        // Cambios mios en master 1
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)

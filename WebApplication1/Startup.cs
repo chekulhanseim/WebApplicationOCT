54f65f944937f0a41db22a66df625dee618d8fb7
chekulhan <46518731+chekulhanseim@users.noreply.github.com>
2019-10-18 11:18:31 +0200

    Upload cambios 22222

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -18,6 +18,7 @@ namespace WebApplication1
         public void ConfigureServices(IServiceCollection services)
         {
         }
+        //Cambios desde TetsProject222222
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
